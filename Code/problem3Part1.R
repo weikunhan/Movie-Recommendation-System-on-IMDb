f9903ed3af0264dd0397c4879765e7349d94a78a
Han::Mark <weikunhan@g.ucla.edu>
2017-05-25 23:17:56 -0700

    Update problem3Part1.R

--- Code/problem3Part1.R
+++ Code/problem3Part1.R
@@ -10,20 +10,20 @@
 library("igraph")
 
 # Setup the file path to load data
-file_path <- "~/Documents/project_2_data/artist_graph.txt"
+file_path <- read.table("~/Documents/project_2_data/artist_graph.txt")
 
 # Load the graph from the path
-graph <- read.graph(file = file_path ,format = "ncol", directed = TRUE)
+graph <- graph.data.frame(file_path, directed = TRUE)
 
 # Run pagerank algorithm
-pr <- page.rank(graph, directed = TRUE, damping = 0.85)
+pr <- page.rank(graph)
 sorted_pr <- sort(pr$vector, decreasing = TRUE, index.return = TRUE)
 
 # Store data into .txt file
-write.table(sorted_pr[[1]], file='pagerank_score.txt', col.names = FALSE)
+write.table(sorted_pr[[1]], file="~/Documents/project_2_data/pagerank_score.txt", col.names = FALSE)
 
 # Print information
-cat("-------------------------Processing Finshed 3----------------------------------\n",
+cat("-------------------------Processing Finshed 1----------------------------------\n",
     "Run pagerank algorithm on the actor/actress network successfully!\n",
     "The first col in the dataset is the ID for the artiest\n",
     "The second col in the dataset is the pagerank score for the artiest\n",
