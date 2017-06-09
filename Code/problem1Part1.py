e377092ef8c63c31596b1dd7dbb6cdb46fc315b8
Han::Mark <weikunhan@g.ucla.edu>
2017-06-08 23:25:05 -0700

    Create problem1Part1.py

--- Code/problem1Part1.py
+++ Code/problem1Part1.py
@@ -55,8 +55,7 @@ with open(filename2, "r") as fp2:
 
         # Check each infomation in selected file in the dataset
         for line in fp2:
-
-        
+  
             # 2 tab in front of moive so make this as conditon
             if(line.count(split) >= 5):
                 file.write(line)
